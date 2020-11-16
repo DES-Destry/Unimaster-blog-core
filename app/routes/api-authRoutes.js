d3bbfa2f10c105cc4410408409886b64c2429b0b
DES-Destry <111andreyaa@gmail.com>
2020-11-16 09:56:52 +0600

    Added documentation for /api/auth endpoints; Server validations taken out to lib/utils js file;

--- app/routes/api-authRoutes.js
+++ app/routes/api-authRoutes.js
@@ -1,16 +1,63 @@
 const controller = require('../../controllers/authController');
-const { check } = require('express-validator');
-
-const regValidate = [
-    check('username', 'Username must be not empty').notEmpty(),
-    check('username', 'Username must be not email').not().isEmail(),
-    check('email', 'Wrong email form').isEmail().normalizeEmail(),
-    check('password', 'Passwords length must be not less than 8 characters').isLength({ min: 8 })
-];
+const { validations } = require('../../lib/utils');
 
 module.exports = (router) => {
-    router.post('/registrate', regValidate, controller.registration);
+    /*
+        -Functional:
+        Create new user, if his email already not use in blog.
+
+        -Usage:
+        Made POST request to  hostname/api/auth/registrate with request body.
+        Request body contains 3 values: "username", "email" and "password".
+        Request body type - JSON. Don't forget for "Content-Type: application/json" header.
+        
+        -Server validation:
+        Username -> not empty and not email.
+        Email -> must be email.
+        Password -> not less than 8 symbols.
+
+        -Example:
+        POST http://localhost:3000/api/auth/registrate
+        Content-Type: application/json
+
+        {
+            "username": "Destry",
+            "email": "test@mail.ru",
+            "password": "12345678"
+        }
+    */
+    router.post('/registrate', validations.registration, controller.registration);
+
+    /*
+        -Functional:
+        Authorization user in blog. In future user can authenticate without authorization.
+
+        -Usage:
+        Made POST request to  hostname/api/auth/login with request body.
+        Request body contains 2 values: "login" and "password".
+        Request body type - JSON. Don't forget for "Content-Type: application/json" header. 
+        In body.login user can write username or email.
+
+        -Example:
+        POST http://localhost:3000/api/auth/login
+        Content-Type: application/json
+
+        {
+            "login": "MyUsername",  //(or "login": "test@gmail.com")
+            "password": "12345678"
+        }
+    */
     router.post('/login', controller.login);
+
+    /*
+        -Functional:
+        Logout user from blog. For future autentication user will need authorize again.
+
+        -Usage: 
+        Made POST request to  hostname/api/auth/logout
+
+        -Example:
+        POST http://localhost:3000/api/auth/logout
+    */
     router.post('/logout', controller.logout);
-    router.get('/test', controller.auth);
 }
