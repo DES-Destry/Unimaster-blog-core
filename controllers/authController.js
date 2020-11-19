24d4c97980dd945fb3adb437f0112f4f8288095b
DES-Destry <111andreyaa@gmail.com>
2020-11-19 15:44:05 +0600

    Refactoring: servers auth endpoints now sends structural response with one standart;

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,65 @@
-const User = require('../models/User');
-const { validationResult } = require('express-validator');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
+const User = require('../models/User');
 const config = require('../lib/config');
+const { objects } = require('../lib/utils');
+
+function validateErrors(req, res){
+    const response = Object.create(objects.serverResponse);
+
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        response.errorMsg = 'Validation error';
+        response.errorType = typeof(Error);
+        response.content = errors.array();
+
+        return res.status(400).json(response);
+    }
+}
 
 module.exports = {
     auth: function(req, res, next){
+        const response = Object.create(objects.serverResponse);
+
         //Authentication with passport-jwt
         passport.authenticate('jwt', (err, user) => {
-            if (err) return res.status(500).send('Unknown authenticate error!');
+            if (err) {
+                response.errorMsg = 'Unknown authenticate error'
+                response.errorType = typeof(Error);
+
+                return res.status(500).json(response);
+            }
 
             if (user){
-                next();
+                return next();
             }
-            else{
-                res.status(401).send('Users data is not correct. Try again or register the user.');
-            }
+
+            response.errorMsg = 'Users data is not correct'
+            response.errorType = typeof(Error);
+            res.status(401).json(response);
+
         })(req, res, next);
     },
 
     registration: async function(req, res){
+        const response = Object.create(objects.serverResponse);
+
         try{
             //Validate user input
-            var errors = validationResult(req);
-            if (!errors.isEmpty()) return res.status(400).send(errors.array());
+            if(validateErrors(req, res)) return;
 
             const { username, email, password } = req.body;
 
             //If such user already exist we can't create new user
             const user = await User.findOne({ email });
-            if (user) return res.status(403).send('User with this email already exists!');
+            if (user){
+                response.errorMsg = 'User with this email already exists';
+                response.errorType = typeof(Error);
+
+                return res.status(403).json(response);
+            }
 
             //Create new user
             const createdUser = new User({ username, email, password });
@@ -40,17 +69,24 @@ module.exports = {
             createdUser.save();
 
             //Form a JSON for send to the client. It contains jwt and user data without password
-            const response = { ...createdUser._doc, ...{ token } };
-            delete response.hPassword;
+            response.msg = 'User has been created'
+            response.content = { ...createdUser._doc, ...{ token } };
+            delete response.content.hPassword;
 
             res.json(response);
         }
         catch(err){
-            res.status(500).send('Unknown server error!');
+            response.errorMsg = err.message;
+            response.errorType = typeof(err);
+            response.content = err;
+
+            res.status(500).json(response);
         }
     },
 
     login: async function(req, res){
+        const response = Object.create(objects.serverResponse);
+
         try{
             const { login, password } = req.body;
 
@@ -64,7 +100,12 @@ module.exports = {
 
                 if (findedUser.checkPass(password)){
                     return req.logIn(findedUser, err => {
-                        if (err) return res.status(500).send('Unknown authenticate error!');
+                        if (err) {
+                            response.errorType = typeof(err);
+                            response.errorMsg = err.message;
+
+                            return res.status(500).json(errRescponse);
+                        }
 
                         //Generate token for sending
                         const email = findedUser.email;
@@ -72,27 +113,42 @@ module.exports = {
                         const token = jwt.sign({ email, username }, config.jwtSecret);
 
                         //Form a JSON for send to the client. It contains jwt and user data without password
-                        const response = { ...findedUser._doc, ...{ token } };
-                        delete response.hPassword;
+                        response.msg = 'Loging in has been successful';
+                        response.content = {
+                            ...findedUser._doc, 
+                            ...{ token }
+                        };
+                        delete response.content.hPassword;
 
-                        return res.send(response);
+                        return res.json(response);
                     });
                 }
 
-                return res.status(401).send('Wrong password!');
+                response.errorMsg = 'User has been founded, but password not correct'
+                response.errorType = typeof(Error);
+                return res.status(401).json(response);
             }
 
-            res.status(401).send('Wrong login!');
+            response.errorMsg = 'User with this login not exists'
+            response.errorType = typeof(Error);
+            res.status(401).json(response);
         }
         catch(err){
-            console.log(err);
-            res.status(500).send('Unknown server error!');
+            response.errorType = typeof(err);
+            response.errorMsg = err.message;
+            response.content = err;
+
+            res.status(500).json(response);
         }
     },
 
     logout: function(req, res){
+        const response = Object.create(objects.serverResponse);
+
         req.logout();
-        delete req.session.passport.user;
-        res.send('Loging out has been successeful!');
+        if (req.session.passport.user) delete req.session.passport.user;
+
+        response.msg = 'Loging out has been successeful'
+        res.json(response);
     }
 }
